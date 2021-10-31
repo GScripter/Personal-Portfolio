16d4670b962af982ce6371d71c2c5624d0168510
gabriel <santosgabriel6763@gmail.com>
2021-10-31 15:38:31 -0200

    Changed ALLOWED_HOSTS

--- Portfolio_Pessoal/settings.py
+++ Portfolio_Pessoal/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['meu-portfolio-9876.herokuapp.com', '*']
+ALLOWED_HOSTS = ['gabrielsantana.herokuapp.com', '*']
 
 
 # Application definition
