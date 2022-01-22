b577df6e1acab7aac54c48b157105a2ce3b96f87
gabriel <santosgabriel6763@gmail.com>
2022-01-22 21:18:06 -0200

    New contact system 📨

--- Portfolio_Pessoal/settings.py
+++ Portfolio_Pessoal/settings.py
@@ -150,3 +150,13 @@ CLOUDINARY_STORAGE  = {
 }
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+# Send Email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+RECIPIENT_ADDRESS = config('RECIPIENT_ADDRESS')
+
--- page/views.py
+++ page/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, TemplateView
 from .models import Project, Knowledge, EEC, Service
 from users.models import User
+from django.core.mail import send_mail
+from django.conf import settings
 
 # Create your views here.
 class AllPageView(ListView):
@@ -17,4 +19,15 @@ class AllPageView(ListView):
         context['service'] = Service.objects.all().order_by('-modified')
         context['procfile'] = User.objects.get(pk=1)
         return context
+
+
+    def post(self, request):
+        organizar = (f"nome: {request.POST['name']}\ne-mail: {request.POST['e-mail']}\nmessage: {request.POST['message']}")
+        send_mail(
+            subject=request.POST['subject'],
+            message=organizar,
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=[settings.RECIPIENT_ADDRESS]
+        )
+        return render(request, 'success.html')
 
